66f4db2db292a5c41ec4f1f4d1535f5639cc565f
hamazin <hademolahamazin@gmail.com>
2025-10-18 15:57:55 +0100

    Inventory model

--- InventoryManagement/API/models.py
+++ InventoryManagement/API/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    name=models.CharField
+    description= models.CharField
+    quantity= models.IntegerField
+    price=models.FloatField
+    createdat= models.DateTimeField
+    Updatedat= models.DateTimeField
