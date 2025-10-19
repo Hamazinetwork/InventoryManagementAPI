6851e8be93a56f3d74aa9d1d99744161d1b24c66
hamazin <hademolahamazin@gmail.com>
2025-10-19 22:05:32 +0100

    CRUD created in views

--- InventoryManagement/API/views.py
+++ InventoryManagement/API/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.views import generic
+from rest_framework import generics
 from models import Inventory
 from Serializers import InventorySerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
-
-class CreateView(generic.CreateView):
+class CreateView(generics.CreateAPIView):
     queryset= Inventory.objects.all()
-    seralizers_class=InventorySerializer.serializers
-    permission_classed= [iaAuthenticatedReadOnly]
+    seralizers_class=InventorySerializer
+    permission_classes= [IsAuthenticatedOrReadOnly]
+
+class DetailsView(generics.RetrieveAPIView):
+    queryset= Inventory.objects.all()
+    seralizers_class=InventorySerializer
+    permission_classes= [IsAuthenticatedOrReadOnly]
+
+class UpdateView(generics.UpdateAPIView):
+    queryset= Inventory.objects.all()
+    seralizers_class=InventorySerializer
+    permission_classes= [IsAuthenticatedOrReadOnly]
+
+class DeleteView(generics.DestroyAPIView):
+    queryset= Inventory.objects.all()
+    seralizers_class=InventorySerializer
+    permission_classes= [IsAuthenticatedOrReadOnly]
+
+class ListView(generics.ListAPIView):
+    queryset= Inventory.objects.all()
+    seralizers_class=InventorySerializer
+    permission_classes= [IsAuthenticatedOrReadOnly]
