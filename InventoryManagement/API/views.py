a78cf5224268cdc9d73f83b4df205c99c9d60f12
hamazin <hademolahamazin@gmail.com>
2025-10-19 21:54:02 +0100

    updated serializers

--- InventoryManagement/API/views.py
+++ InventoryManagement/API/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from models import Inventory
+from Serializers import InventorySerializer
 
-# Create your views here.
+
+
+class CreateView(generic.CreateView):
+    queryset= Inventory.objects.all()
+    seralizers_class=InventorySerializer.serializers
+    permission_classed= [iaAuthenticatedReadOnly]
