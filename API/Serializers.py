cd79cab7833e6de86fd923609ad5a5d1504e7f5e
hamazin <hademolahamazin@gmail.com>
2025-10-21 22:08:47 +0100

    added validation to serilaizers(name)

--- API/Serializers.py
+++ API/Serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from models import quantity, description, createdat, updatedat, name, price
+from models import Inventory
 
 class InventorySerializer(serializers.ModelSerializer):
     class Meta:
-        model=MyModal
+        model= Inventory
         field=[id, quantity,description, createdat, updatedat,name, price]
+
+    def validate(self, data):
+        if len(data['title']) < 5:
+            raise serializers.ValidationError("Enter atleast 5 characters")
+        return data
--- InventoryManagement/API/Serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from models import quantity, description, createdat, updatedat, name, price
-
-class InventorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=MyModal
-        field=[id, quantity,description, createdat, updatedat,name, price]
