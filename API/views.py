31e7f3fa9bef45e2a6bf1b262466c201ac510d11
hamazin <hademolahamazin@gmail.com>
2025-10-21 22:19:28 +0100

    custom pagination created and added to views

--- API/views.py
+++ API/views.py
@@ -4,6 +4,7 @@ from models import Inventory
 from Serializers import InventorySerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from pagination import CustomPagination
 
 
 class CreateView(generics.CreateAPIView):
@@ -11,6 +12,7 @@ class CreateView(generics.CreateAPIView):
     seralizers_class=InventorySerializer
     permission_classes= [IsAuthenticatedOrReadOnly]
     authentication_classes=[TokenAuthentication]
+    pagination_class= CustomPagination
 
 
 class DetailsView(generics.RetrieveAPIView):
@@ -18,21 +20,26 @@ class DetailsView(generics.RetrieveAPIView):
     seralizers_class=InventorySerializer
     permission_classes= [IsAuthenticatedOrReadOnly]
     authentication_classes=[TokenAuthentication]
+    pagination_class= CustomPagination
 
 class UpdateView(generics.UpdateAPIView):
     queryset= Inventory.objects.all()
     seralizers_class=InventorySerializer
     permission_classes= [IsAuthenticatedOrReadOnly]
     authentication_classes=[TokenAuthentication]
+    pagination_class= CustomPagination
+
 
 class DeleteView(generics.DestroyAPIView):
     queryset= Inventory.objects.all()
     seralizers_class=InventorySerializer
     permission_classes= [IsAuthenticatedOrReadOnly]
     authentication_classes=[TokenAuthentication]
+    pagination_class= CustomPagination
 
 class ListView(generics.ListAPIView):
     queryset= Inventory.objects.all()
     seralizers_class=InventorySerializer
     permission_classes= [IsAuthenticatedOrReadOnly]
     authentication_classes=[TokenAuthentication]
+    pagination_class= CustomPagination
